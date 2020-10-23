7531f567a2ec5865468248f1be46dbe9b85d9fd6
Ji-Chun Lian <jichunlian@hnu.edu.cn>
2020-10-23 20:49:34 +0800

    Update stdout.f90

--- src/stdout.f90
+++ src/stdout.f90
@@ -108,7 +108,7 @@ subroutine stdout_5(time)
   write(*,'(A)') '|                     * How to Site ? *                    |'
   write(*,'(A)') '| Please Cite The Following Article When You Use disorder: |'
   write(*,'(A)') '| [1] Ji-Chun Lian, Hong-Yu Wu, Wei-Qing Huang, Wangyu Hu, |'
-  write(*,'(A)') '|    and Gui-Fang Huang, Phys. Rev. B, 102, 134209 (2020). |'
+  write(*,'(A)') '|     and Gui-Fang Huang, Phys. Rev. B 102, 134209 (2020). |'
   write(*,'(A)') '+----------------------------------------------------------+'
   write(*,*)
   return
